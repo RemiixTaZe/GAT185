71357e721427d32d052e7241463a47aa4b5e9ba8
RemiixTaZe <Sirdracius926@gmail.com>
2021-01-13 14:47:59 -0700

    Yea this is a thing

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            GameObject gameObject = Instantiate(bullet, transform.position, Quaternion.identity);
+            gameObject.GetComponent<Bullet>().Fire(ray.direction);
+            
+        }
+    }
+}
