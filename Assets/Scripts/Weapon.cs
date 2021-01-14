1b327a6bf892ca827aa8b0eb3bad134ecd1c5ed6
RemiixTaZe <Sirdracius926@gmail.com>
2021-01-14 14:32:13 -0700

    Added some stuff
    Added -
    Target and target materials
    Game script
    target script
    Edited player and weapon scripts
    Added target materials and textures
    
    Changed where things were in scripts

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public float fireRate = 0.1f;
+    
+
+    int ammo = 100;
+    float fireTimer = 0;
+
     public GameObject bullet;
 
     // Start is called before the first frame update
@@ -15,13 +21,32 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        fireTimer += Time.deltaTime;
 
-            GameObject gameObject = Instantiate(bullet, transform.position, Quaternion.identity);
-            gameObject.GetComponent<Bullet>().Fire(ray.direction);
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //    GameObject gameObject = Instantiate(bullet, transform.position, Quaternion.identity);
+        //    gameObject.GetComponent<Bullet>().Fire(ray.direction);
             
+        //}
+    }
+
+    public bool Fire(Vector3 position, Vector3 direction)
+    {
+
+        if(fireTimer >= fireRate)
+        {
+            fireTimer = 0;
+            GameObject gameObject = Instantiate(bullet, position, Quaternion.identity);
+            gameObject.GetComponent<Bullet>().Fire(direction);
+            return true;
         }
+        else
+        {
+            return false;
+        }
+
     }
 }
